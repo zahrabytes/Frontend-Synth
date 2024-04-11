fbcb38a597fd97c1226287c8f1d4431c8e80cab2
kevinm000 <163909246+kevinm000@users.noreply.github.com>
2024-04-10 20:00:55 -0500

    pushing

--- synth/src/Pages/login-artist.jsx
+++ synth/src/Pages/login-artist.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'axios'; // Import Axios for making HTTP requests
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
