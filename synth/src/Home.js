a876cffddc7a4e4173c55717ef76f0bbf83a8e33
Zahra Bukhari <bukhaz99@gmail.com>
2024-04-15 15:54:38 -0500

    added a cool bg /Test

--- synth/src/Home.js
+++ synth/src/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './index.css'
-
+import './home.css'
 function Home () {
-    return (
-        <div>
+  return (
+    <div className='main'>
+        <div className="overlay"></div>
+        <img src='https://storage.googleapis.com/bucket-tester-2/youtube-video-gif.gif'alt="GIF"/>
+        <div className="content">
             <div className="hover">
-                <header>Synth</header>
+                <homeheader>Synth</homeheader>
             </div>
         </div>
-    );
+    </div>
+  )
 }
- 
+
 export default Home;
