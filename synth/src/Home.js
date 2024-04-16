bd39951befb072ac962c5b0a1da7b1baa782da23
Zahra Bukhari <bukhaz99@gmail.com>
2024-04-16 15:01:05 -0500

    just styling

--- synth/src/Home.js
+++ synth/src/Home.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './home.css'
 function Home () {
   return (
-    <div-landing>
+    <div className="div-landing">
     <div className='main-landing'>
         <div className="overlay-landing"></div>
-        <img-landing src='https://storage.googleapis.com/bucket-tester-2/youtube-video-gif.gif'alt="GIF"/>
+        <img className='.img-landing' src='https://storage.googleapis.com/bucket-tester-2/youtube-video-gif.gif'alt="GIF"/>
         <div className="content-landing">
             <div className="hover-landing">
                 <homeheader-landing>Synth</homeheader-landing>
             </div>
         </div>
     </div>
-    </div-landing>
+    </div>
   )
 }
 
