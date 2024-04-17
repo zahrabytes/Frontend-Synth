07c1bbe2a768b863b57b0cee0f1528c6c457331a
Zahra Bukhari <bukhaz99@gmail.com>
2024-04-16 19:05:41 -0500

    tried to add a cool homepage but i gave up loll

--- synth/src/App.js
+++ synth/src/App.js
@@ -22,7 +22,7 @@ import LoginAdmin from './Pages/login-admin.jsx';
 import LoginArtist from './Pages/login-artist.jsx';
 import ListenerLogin from './Pages/login-listener.jsx';
 import ViewArtist from "./Listener/ViewArtist.js"
-import Home from './Home.js';
+///import Home from './IsolatedCSS/Home.js';
 
 function App() {
   return (
@@ -31,7 +31,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Landing />} />
-            <Route path="/Test" element={<Home />} />
+            {/*<Route path="/Test" element={<Home />} />*/}
             <Route path="/Create-Album" element={<AlbumAdd />} />
             <Route path="/:id/user-home" element={<ListenerHome />} />
             <Route path="/:id/Admin-Home" element={<AdminHome />} />
---
+++ synth/src/IsolatedCSS/Home.js
@@ -0,0 +1,16 @@
+/*import React from 'react';
+import './home.css';
+
+function Home () {
+  return (
+    <div className='home-container'>
+        <div className="overlay"></div>
+        <img src='https://storage.googleapis.com/bucket-tester-2/youtube-video-gif.gif'alt="GIF"/>
+        <div className="content">
+          <header>Synth</header>
+        </div>
+    </div>
+  )
+}
+
+export default Home;*/
