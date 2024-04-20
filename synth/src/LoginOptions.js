bf168c312aa6f5e7703a2c151763e683d5eedd2d
kevinm000 <163909246+kevinm000@users.noreply.github.com>
2024-04-20 00:11:59 -0500

    updates to Login dropdown menu

--- synth/src/LoginOptions.js
+++ synth/src/LoginOptions.js
@@ -13,12 +13,12 @@ const LoginOptions = () => {
             <h1 className="TryNow">Try it now</h1>
             <div className="options-banner">
                 <Link to="/registration" className="option">Register</Link>
-                <button className="option" onClick={toggleDropdown}>Login</button>
+                <button className="dropdown-button" onClick={toggleDropdown}>Login</button>
                 {showDropdown && (
                     <div className="dropdown-menu">
-                        <Link to="/login-listener" className="option">Login as Listener</Link>
-                        <Link to="/login-artist" className="option">Login as Artist</Link>
-                        <Link to="/login-admin" className="option">Login as Admin</Link>
+                        <Link to="/login-listener" className="option1">Login as Listener</Link>
+                        <Link to="/login-artist" className="option1">Login as Artist</Link>
+                        <Link to="/login-admin" className="option1">Login as Admin</Link>
                     </div>
                 )}
             </div>
