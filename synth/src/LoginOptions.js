e509ad1a2313a0e2604f7ed96c55b7f3099c746d
kevinm000 <163909246+kevinm000@users.noreply.github.com>
2024-04-19 23:41:37 -0500

    drop down menu login

--- synth/src/LoginOptions.js
+++ synth/src/LoginOptions.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginOptions = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
     return (
         <div>
             <h1 className="TryNow">Try it now</h1>
             <div className="options-banner">
                 <Link to="/registration" className="option">Register</Link>
-                <Link to="/login-listener" className="option">Login as Listener</Link>
-                <Link to="/login-artist" className="option">Login as Artist</Link>
-                <Link to="/login-admin" className="option">Login as Admin</Link>
+                <button className="option" onClick={toggleDropdown}>Login</button>
+                {showDropdown && (
+                    <div className="dropdown-menu">
+                        <Link to="/login-listener" className="option">Login as Listener</Link>
+                        <Link to="/login-artist" className="option">Login as Artist</Link>
+                        <Link to="/login-admin" className="option">Login as Admin</Link>
+                    </div>
+                )}
             </div>
         </div>
     );
