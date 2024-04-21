f986f7ea67c0e1d37d029eade62e1aa5c19ed4db
Zahra Bukhari <bukhaz99@gmail.com>
2024-04-20 20:11:30 -0500

    i changed some album view styyling and also did a lot of artist report stufff

--- synth/src/Artist/Reports.jsx
+++ synth/src/Artist/Reports.jsx
@@ -15,7 +15,7 @@ import {
     Tooltip
 } from "chart.js/auto";
 import 'chartjs-plugin-datalabels';
-import { Bar, Doughnut } from 'react-chartjs-2';
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -24,15 +24,33 @@ const TestReport = () => {
     const [genderList, setGenderList] = useState([]);
     const [ageReport, setAgeReport] = useState([]);
     const [ageList, setAgeList] = useState([]);
-    const [timestamp, setTimestamp] = useState([]);
+    const [followerTimestamp, setFollowerTimestamp] = useState([]);
     const [followersListeners, setFollowersListeners] = useState([]);
-    const [doughnutData, setDoughnutData] = useState('genderReport');
-    const [selected,setSelected]=useState('Gender of Followers');
+    const [selectedDoughnut,setSelectedDoughnut]=useState('');
+    const [selectFirstTable,setSelectFirstTable]=useState('');
+    const [selectBar,setSelectBar]=useState('');
+    const [selectLine,setSelectLine]=useState('');
 
     const handleChange=(e)=>{
-        console.log(e.target.value)
-        setSelected(e.target.value)
-    }
+        console.log(e.target.value);
+        setSelectedDoughnut(e.target.value);
+    };
+
+    const handleFirstTableChange=(e)=>{
+        console.log(e.target.value);
+        setSelectFirstTable(e.target.value);
+    };
+
+    const handleBarChange=(e)=>{
+        console.log(e.target.value);
+        setSelectBar(e.target.value);
+    };
+
+    const handleLineChange=(e)=>{
+        console.log(e.target.value);
+        setSelectLine(e.target.value);
+    };
+
 
     const navigate = useNavigate();
 
@@ -52,7 +70,7 @@ const TestReport = () => {
                 setAgeList(aList.data);
 
                 const timestamp = await axios.get(`http://localhost:8000/artist-timestamp`);
-                setTimestamp(timestamp.data);
+                setFollowerTimestamp(timestamp.data);
 
                 const follow = await axios.get(`http://localhost:8000/artist-follower-listener`);
                 setFollowersListeners(follow.data);
@@ -146,6 +164,32 @@ const TestReport = () => {
             ],labels: ageReport.map((data) => data.age_bracket),
       };
 
+      const dataFollowerTimestamp= {
+        datasets: [
+            {
+                data: followerTimestamp.map((data) => data.followers_gained),
+                borderColor: [
+                    'rgb(227,232,236)',
+                ],
+            },
+            
+        ], labels: followersListeners.map((data) => data.date),
+      };
+
+      const optionsLine = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+          },
+        },
+      };
+      
+
   
 
     //   dataTimestamp
@@ -154,17 +198,30 @@ const TestReport = () => {
             <artistName>Artist Dashboard</artistName>
             <h1>Listener Demographics</h1>
             <h2>Get to know your listeners through statistical reports.</h2>
-            <div className='flex-container'>
+            <div>
             <div className='option-doughnut-container'>
-                <select value={selected} onChange={(e)=>handleChange(e)}>
+                <select value={selectedDoughnut} onChange={(e)=>handleChange(e)}>
+                    <option>Select Option</option>
                     <option>Gender of Your Followers as Percentages</option>
                     <option>Percent of Synth Listeners Who Follow You</option>
                 </select>
                 <div className='chart-container'>
-                    {selected === "Gender of Your Followers as Percentages"?<div>
-                        <Doughnut options={optionsGender} data={dataGenderFollower} />
-                                <table className='scrollbar-table'>
-                                <h2>Your Followers And Their Gender:</h2>
+                    {selectedDoughnut === "Gender of Your Followers as Percentages"?<div>
+                        <Doughnut options={optionsGender} data={dataGenderFollower} /></div>:""}
+                    {selectedDoughnut === "Percent of Synth Listeners Who Follow You"?<div>
+                        <Doughnut options={optionsGender} data={dataFollowerListener} /></div>:"" }
+                </div>
+            </div> 
+            <div className='option-doughnut-container'>
+                <select value={selectFirstTable} onChange={(e)=>handleFirstTableChange(e)}>
+                    <option>Select Option</option>
+                    <option>Your Followers And Their Gender</option>
+                    <option>Your Followers Compared to All Listeners</option>
+                </select>
+                <div className='chart-container'>
+                    {selectFirstTable === "Your Followers And Their Gender"?<div>
+                            <table className='scrollbar-table'>
+                                <h2>Your Followers And Their Genders:</h2>
                                     <thead>
                                     <tr>
                                         <th>First Name</th>
@@ -184,13 +241,15 @@ const TestReport = () => {
                                     ))}
                                     </tbody>
                                 </table></div>:""}
-                    {selected === "Percent of Synth Listeners Who Follow You"?<div><Doughnut options={optionsGender} data={dataFollowerListener} />
-                    <table>
-                                <h2>Your Followers Compared to All Listeners:</h2>
+                    {selectFirstTable === "Your Followers Compared to All Listeners"?
+                    <div>
+                            <table>
+                                <h2>Your Followers
+                                    Compared to All Listeners:</h2>
                                     <tbody className='scrollbar-table'>
                                     {followersListeners.map((follower, index) => (
                                         <tr key={index}>
-                                        <td>{follower.label}</td>
+                                        <td>{follower.label}: </td>
                                         <td>{follower.percent}</td>
                                         </tr>
                                     ))}
@@ -198,11 +257,63 @@ const TestReport = () => {
                                 </table></div>:"" }
                 </div>
             </div> 
+            </div>
+            <div >
+                <select value={selectBar} onChange={(e)=>handleBarChange(e)}>
+                    <option>Select Option</option>
+                    <option>Graph of Your Followers in Each Age Bracket</option>
+                    <option>Count of Your Followers in Each Age Bracket</option>
+                    <option>Your Followers And Their Ages</option>
+                </select>
                 <div className='bar-chart-container'>
-                    <Bar options={optionsBar} data={dataAgeReport}/>
+                    {selectBar === "Graph of Your Followers in Each Age Bracket"?<div>
+                        <Bar options={optionsBar} data={dataAgeReport}/></div>:""}
+                    {selectBar === "Count of Your Followers in Each Age Bracket"?<div>
+                    <table className='scrollbar-table'>
+                                <h2>Count of Your Followers in Each Age Bracket:</h2>
+                                    <thead>
+                                    <tr>
+                                        <th>Bracket</th>
+                                        <th>Follower Count</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {ageReport.map((item, index) => (
+                                        <tr key={index}>
+                                        <td>{item.age_bracket}</td>
+                                        <td>{item.followers_count}</td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </table></div>:"" }
+                    {selectBar === "Your Followers And Their Ages"?<div>
+                    <table className='scrollbar-table'>
+                        <h2>Your Followers And Their Ages:</h2>
+                            <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                                <th>Age</th>
+                            </tr>
+                            </thead>
+                                <tbody>
+                                {ageList.map((follower, index) => (
+                                    <tr key={index}>
+                                        <td>{follower.fname}</td>
+                                        <td>{follower.lname}</td>
+                                        <td>{follower.username}</td>
+                                        <td>{follower.age}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                    </table>
+                    </div>:"" }
                 </div>
-        </div>
-        </div>
+        </div> 
+        <div className='bar-chart-container'>
+        <Line options={optionsLine} data={dataFollowerTimestamp} /></div>
+    </div>
     );
 };
 
